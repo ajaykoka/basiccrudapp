34efad2dc16d442502d32537971a458c523e6156
Sri Sathya Ajay Koka <ajaykoka1704@gmail.com>
2023-06-21 12:58:36 +0530

    basic crud app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-function App() {
+const API_URL = 'https://jsonplaceholder.typicode.com/posts';
+
+const App = () => {
+  const [posts, setPosts] = useState([]);
+  const [newPost, setNewPost] = useState('');
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  const fetchPosts = async () => {
+    try {
+      const response = await axios.get(API_URL);
+      setPosts(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const createPost = async () => {
+    try {
+      const response = await axios.post(API_URL, { title: newPost });
+      setPosts([...posts, response.data]);
+      setNewPost('');
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const deletePost = async (id) => {
+    try {
+      await axios.delete(`${API_URL}/${id}`);
+      const updatedPosts = posts.filter((post) => post.id !== id);
+      setPosts(updatedPosts);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-4">
+      <h1>CRUD App</h1>
+
+      <div className="mb-3">
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Enter a new post"
+          value={newPost}
+          onChange={(e) => setNewPost(e.target.value)}
+        />
+        <button className="btn btn-primary mt-2" onClick={createPost}>
+          Add Post
+        </button>
+      </div>
+
+      <ul className="list-group">
+        {posts.map((post) => (
+          <li className="list-group-item" key={post.id}>
+            {post.title}
+            <button
+              className="btn btn-danger btn-sm float-end"
+              onClick={() => deletePost(post.id)}
+            >
+              Delete
+            </button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
 
 export default App;
